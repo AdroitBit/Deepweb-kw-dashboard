85e2ed6e3972f7e71c136a653ee1221d15f7cddd
AdroitBit <yanothaic15@gmail.com>
2024-04-04 11:32:03 +0700

    test empty post request

--- src/1-depth1-link-gatherer/test.py
+++ src/1-depth1-link-gatherer/test.py
@@ -7,13 +7,16 @@ proxies = {
 }
 
 # URL of the Onion website you want to access
-onion_url = 'http://jgwe5cjqdbyvudjqskaajbfibfewew4pndx52dye7ug3mt3jimmktkid.onion/'
-onion_url = 'http://xjfbpuj56rdazx4iolylxplbvyft2onuerjeimlcqwaihp3s6r4xebqd.onion/'
+# onion_url = 'http://jgwe5cjqdbyvudjqskaajbfibfewew4pndx52dye7ug3mt3jimmktkid.onion/'
+# onion_url = 'http://xjfbpuj56rdazx4iolylxplbvyft2onuerjeimlcqwaihp3s6r4xebqd.onion/'
+
+
+#breach data
 onion_url = "http://breachdbsztfykg2fdaq2gnqnxfsbj5d35byz3yzj73hazydk4vq72qd.onion/"
 
 # Make a GET request using the SOCKS proxy
 # response = requests.post(onion_url, proxies=proxies)
-response = requests.get(onion_url, proxies=proxies)
+response = requests.post(onion_url, proxies=proxies)
 
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
